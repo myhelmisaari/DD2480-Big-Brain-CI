45e3a5f709edff16c6756579b8e0e693068ab194
MathieuDesponds <mathieu.desponds@epfl.ch>
2020-02-02 18:44:49 +0100

    Fix Solves #12 Redo the Structure of the project adding Gradle
    Add Gradle to the project. The project can be build with `gradlew
    build`. Gradle files as `build.gradle`, `gradlew`, `gradlew.bat`,
    `settings.gradle` have been added.
    I put the `ContinuousIntegrationServer` class in the src file.

--- src/main/java/ContinuousIntegrationServer.java
+++ src/main/java/ContinuousIntegrationServer.java
@@ -1,12 +1,11 @@
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
- 
 import java.io.IOException;
- 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.handler.AbstractHandler;
 
 /** 
  Skeleton of a ContinuousIntegrationServer which acts as webhook
