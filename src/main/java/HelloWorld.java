351baf203d861dfce48ec11a5202d7f887937d3a
myhelmisaari <myhelmisaari@gmail.com>
2020-02-07 09:46:45 +0100

    test if build will fail

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String [] args){
-        System.out.println("HelloWorld");
+        System..println("HelloWorld");
     }
     public static int returnOne(){
         return 1;
