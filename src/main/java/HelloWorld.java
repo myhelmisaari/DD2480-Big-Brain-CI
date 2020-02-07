895dab87df825fe22b0a8eb39133d2c229f2732f
myhelmisaari <myhelmisaari@gmail.com>
2020-02-07 09:43:06 +0100

    test

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String [] args){
-        System.out.println("HelloWorld");
+        System.ot.println("HelloWorld");
     }
     public static int returnOne(){
         return 1;
