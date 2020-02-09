ba133dff490b4e6d6a4b3f60a5abcad77e77cbde
unknown <hmellin@kth.se>
2020-02-09 19:05:06 +0100

    testing

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String [] args){
-        System.out.println("HelloWorld");
+        System.out.println("HelloasdasdasdWorld");
     }
     public static int returnOne(){
         return -1;
