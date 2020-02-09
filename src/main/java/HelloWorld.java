496eeb8b4323b9596a38d774b5ae7a3b7f17ae8d
myhelmisaari <myhelmisaari@gmail.com>
2020-02-09 17:41:46 +0100

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String [] args){
-        System.out.println("HelloWorld");
+        System.out.println("HellWorld");
     }
     public static int returnOne(){
         return 1;
